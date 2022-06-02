79d02f578839481ebcb9d457ef6f150c6c988d2f
Gaurav Gunjal <gaurav.gunjal@ic.gumgum.com>
2022-06-02 12:33:16 +0530

    Counter - setState

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 import Greet from './components/Greet';
 import Welcome from './components/Welcome';
 import Message from './components/Message';
+import Counter from './components/Counter';
 
 function App() {
   return (
     <div className="App">
       {/* <Greet name="Gaurav"></Greet>
       <Welcome name="Gaurav"></Welcome> */}
-      <Message></Message>
+      <Counter></Counter>
     </div>
   );
 }
